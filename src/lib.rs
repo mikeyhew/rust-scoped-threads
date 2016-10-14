f25d46aed82ae7e5bdc31df2836fc4756d9f40b4
Michael Hewson <michael@michaelhewson.ca>
2016-10-14 12:15:45 -0400

    got it mostly working but I still don't get why the spawn closure needs to be put in a box

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,62 @@
+use std::thread;
 use std::thread::JoinHandle;
 use std::collections::LinkedList;
+use std::marker::PhantomData;
+use std::boxed::Box;
 
-pub struct Scope {
-  join_handles: LinkedList<JoinHandle<()>>,
+pub struct Scope<'a> {
+    join_handles: LinkedList<JoinHandle<()>>,
+    _marker: PhantomData<::std::cell::Cell<&'a mut ()>>
 }
 
-pub fn scope<F, R>(f: F) -> R where F: FnOnce(&Scope) -> R {
-  let mut scope = Scope { join_handles: LinkedList::new()};
-  let ret = f(&mut scope);
-  for join_handle in scope.join_handles {
-    join_handle.join().unwrap();
-  }
-  ret
+impl<'a> Scope<'a> {
+    pub fn spawn<F>(&mut self, f: F) where F: FnOnce() + Send + 'a {
+        let closure: Box<FnBox + 'a> = Box::new(f);
+        let closure: Box<FnBox + Send> = unsafe{
+            std::mem::transmute(closure)
+        };
+        let join_handle = thread::spawn(move || closure.call_box());
+        self.join_handles.push_back(join_handle);
+    }
 }
 
+pub fn scope<'a, F, R>(f: F) -> R where F: FnOnce(&mut Scope<'a>) -> R {
+    let mut scope = Scope {
+        join_handles: LinkedList::new(),
+        _marker: PhantomData
+    };
+    let ret = f(&mut scope);
+    for join_handle in scope.join_handles {
+        join_handle.join().unwrap();
+    }
+    ret
+}
+
+trait FnBox {
+    fn call_box(self: Box<Self>);
+}
+
+impl<F: FnOnce()> FnBox for F {
+    fn call_box(self: Box<Self>) { (*self)() }
+}
 
 #[cfg(test)]
 mod tests {
-  use super::*;
-    #[test]
-    fn it_works() {
-      let one = scope(|_|{1});
-      assert!(one == 1);
-    }
+    use super::*;
+        #[test]
+        fn it_works() {
+            let one = scope(|_|{1});
+            assert!(one == 1);
+        }
+
+        #[test]
+        fn one_thread() {
+            let mut foo = 0;
+            scope(|mut scope| {
+                scope.spawn(|| {
+                    foo = 1;
+                });
+            });
+            assert!(foo == 1);
+        }
 }
