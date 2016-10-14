1f0c92ce15af56e5bfb59592401163b750486b1f
Michael Hewson <michael@michaelhewson.ca>
2016-10-13 21:59:22 -0400

    started

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,26 @@
+use std::thread::JoinHandle;
+use std::collections::LinkedList;
+
+pub struct Scope {
+  join_handles: LinkedList<JoinHandle<()>>,
+}
+
+pub fn scope<F, R>(f: F) -> R where F: FnOnce(&Scope) -> R {
+  let mut scope = Scope { join_handles: LinkedList::new()};
+  let ret = f(&mut scope);
+  for join_handle in scope.join_handles {
+    join_handle.join().unwrap();
+  }
+  ret
+}
+
+
 #[cfg(test)]
 mod tests {
+  use super::*;
     #[test]
     fn it_works() {
+      let one = scope(|_|{1});
+      assert!(one == 1);
     }
 }
